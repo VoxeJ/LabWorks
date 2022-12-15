788eaa70db0f1fc8c445f550f4650721077c0a21
Yuzariy <voxelj@gmail.com>
2022-12-15 09:30:51 +0300

    Delete Лаб-4 directory

--- Лаб-4/N1/N1P.pl
+++
@@ -1,6 +0,0 @@
-man(giosciamus,otlichnik).
-man(vyokh,troechnik).
-woman(samantha,horoshist).
-woman(prokata,troechnik).
-molodtsy(M):-man(M,otlichnik);man(M,horoshist);woman(M,otlichnik);woman(M,horoshist).
-otvet(M):-molodtsy(M),man(M,_).
